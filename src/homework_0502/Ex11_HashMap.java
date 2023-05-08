e283517d7f164620f8e59100f3e3c33712df7ccd
ZZZZZZZIWON <pjw2012043@gmail.com>
2023-05-08 19:19:55 +0900

    0508 19:19

--- src/homework_0502/Ex11_HashMap.java
+++ src/homework_0502/Ex11_HashMap.java
@@ -3,9 +3,11 @@ package homework_0502;
 import java.util.*;
 
 public class Ex11_HashMap {
-        HashMap<String, String> h;
+    HashMap<String, String> h;
 
     public Ex11_HashMap() {
+        // 생성자 method를 이용 ->  나라와 수도 정보 저장
+        // 각각의 key값은 country, value값은 capital을 저장
         h = new HashMap<String, String>(10);
         h.put("체코", "프라하");
         h.put("영국", "런던");
@@ -17,53 +19,90 @@ public class Ex11_HashMap {
         h.put("오스트리아", "빈");
         h.put("그리스", "아테네");
     }
+
+    // Method 1 ] 나라와 수도를 입력받아서 HashMap에 추가
     private void input() {
         Scanner scan = new Scanner(System.in);
-        System.out.println("현재 "+ h.size() +"개 나라와 수도가 입력되어 있습니다");
-        while(true) {
+        // size() method를 이용해서 현재 HashMap에 저장되어 있는 정보들의 수를 파악
+        System.out.println("현재 " + h.size() + "개 나라와 수도가 입력되어 있습니다");
+        while (true) {
             System.out.print("나라와 수도 입력 " + (h.size() + 1) + ">>");
+            // 공백을 기준으로 첫 번째 nextToken은 country 변수에 저장
             StringTokenizer st = new StringTokenizer(scan.nextLine(), " ");
             String country = st.nextToken();
 
-            if(country.equals("그만")) {
+            // 첫 번째 입력받은 nextToken이 "그만"과 일치할 시 입력 종료
+            if (country.equals("그만")) {
                 System.out.println("입력을 종료합니다");
                 break;
             }
+            // 공백 뒤 두 번째 token은 capital 변수에 저장
             String capital = st.nextToken();
 
-            if(h.containsKey(country)) {
+            // HashMap에 이미 존재하는 나라와 수도는 pass
+            if (h.containsKey(country)) {
                 System.out.println(country + "은/는 이미 입력된 나라입니다");
                 continue;
             }
+            // HashMap에 없다면 put() method를 이용해서 정보 추가
             h.put(country, capital);
         }
     }
 
+    // Method 2 ] 랜덤하게 문제 생성 -> 입력받은 답과 비교
     private void quiz() {
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random();
+        // h의 key값들을 toArray() 메소드를 사용해서 Object[] 배열로 반환
+        Object[] keys = h.keySet().toArray();
+        int index;
+        String country;
+        String capital;
 
+        while(true) {
+            // h에 저장된 key-value 개수의 랜덤한 정수를 index에 저장
+            index = rand.nextInt(h.size());
+            // 랜덤한 index가 가리키는 배열의 값(key)를 country 변수에 저장
+            // keys는 Object[] type이기 때문에 String으로 downcasting
+            country = (String) keys[index];
+            System.out.print(country + "의 수도는? ");
+            // 입력받은 답을 capital에 저장
+            capital = scan.nextLine();
+
+            if (capital.equals("그만")) {
+                System.out.println("퀴즈를 종료합니다.");
+                break;
+            }
+
+            // 입력받은 capital과 country에 담긴 랜덤한 index가 가리키는 배열의 값이 일치할 때
+            if (capital.equals(h.get(country))) {
+                System.out.println("정답");
+            } else {
+                System.out.println("오답입니다");
+            }
+        }
     }
 
     public static void main(String[] args) {
+        // 게임을 진행할 객체 생성
         Ex11_HashMap ex = new Ex11_HashMap();
 //        System.out.println(ex.h.entrySet());
         System.out.println("===== 수도 맞추기 게임을 시작합니다 =====");
 
+        // 입력받는 숫자를 저장할 gameNum 변수
         int gameNum = 0;
         Scanner scan = new Scanner(System.in);
-        while(true) {
-        System.out.println("입력: 1    퀴즈: 2   종료: 3");
-        gameNum = scan.nextInt();
-            if(gameNum == 3) {
+        while (true) {
+            System.out.println("입력: 1    퀴즈: 2   종료: 3");
+            gameNum = scan.nextInt();
+            if (gameNum == 3) {
                 System.out.println("게임을 종료합니다");
                 break;
-            }
-            else if (gameNum == 2) {
+            } else if (gameNum == 2) {
                 ex.quiz();
-            }
-            else if (gameNum == 1) {
+            } else if (gameNum == 1) {
                 ex.input();
-            }
-            else {
+            } else {
                 System.out.println("다시 입력하세요");
             }
         }
--- src/homework_0502/Ex11_Vector.java
+++ src/homework_0502/Ex11_Vector.java
@@ -1,5 +1,120 @@
 package homework_0502;
 
-public class Ex11_Vector {
+import java.util.Random;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.Vector;
 
+public class Ex11_Vector {
+    // Vector를 생성
+    Vector<String> countries = new Vector<String>();
+    Vector<String> capitals = new Vector<String>();
+
+    public Ex11_Vector() {
+        // 생성자 method를 이용 -> 나라와 수도 정보 저장
+        countries.add("체코");
+        capitals.add("프라하");
+        countries.add("영국");
+        capitals.add("런던");
+        countries.add("독일");
+        capitals.add("베를린");
+        countries.add("프랑스");
+        capitals.add("파리");
+        countries.add("태국");
+        capitals.add("방콕");
+        countries.add("미국");
+        capitals.add("워싱턴");
+        countries.add("스위스");
+        capitals.add("제네바");
+        countries.add("오스트리아");
+        capitals.add("빈");
+        countries.add("그리스");
+        capitals.add("아테네");
+    }
+
+    // Method 1 ] 나라와 수도를 입력받아서 Vector에 추가
+    private void input() {
+        Scanner scan = new Scanner(System.in);
+        // size() method를 이용해서 현재 Vector에 저장되어 있는 정보들의 수를 파악
+        System.out.println("현재 " + countries.size() + "개 나라와 수도가 입력되어 있습니다");
+        while (true) {
+            System.out.print("나라와 수도 입력 " + (countries.size() + 1) + ">>");
+            // 공백을 기준으로 첫 번째 nextToken은 country 변수에 저장
+            StringTokenizer st = new StringTokenizer(scan.nextLine(), " ");
+            String country = st.nextToken();
+
+            // 첫 번째 입력받은 nextToken이 "그만"과 일치할 시 입력 종료
+            if (country.equals("그만")) {
+                System.out.println("입력을 종료합니다");
+                break;
+            }
+            // 공백 뒤 두 번째 token은 capital 변수에 저장
+            String capital = st.nextToken();
+
+            // Vector에 이미 존재하는 나라와 수도는 pass
+            if (countries.contains(country)) {
+                System.out.println(country + "은/는 이미 입력된 나라입니다");
+                continue;
+            }
+            // Vector에 추가
+            countries.add(country);
+            capitals.add(capital);
+        }
+    }
+
+    private void quiz() {
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random();
+        int index;
+        String country;
+        String capital;
+
+        while (true) {
+            // countries Vector의 크기를 이용해 랜덤한 index를 구한다
+            index = rand.nextInt(countries.size());
+            // index에 해당하는 나라를 country 변수에 저장
+            country = countries.get(index);
+            System.out.print(country + "의 수도는? ");
+            capital = scan.nextLine();
+
+            if (capital.equals("그만")) {
+                System.out.println("퀴즈를 종료합니다.");
+                break;
+            }
+
+            // 입력받은 capital과 해당 나라의 수도를 비교하여 결과 출력
+            if (capital.equals(capitals.get(index))) {
+                System.out.println("정답");
+            } else {
+                System.out.println("오답입니다");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        // 게임을 진행할 객체 생성
+        Ex11_Vector ex = new Ex11_Vector();
+
+        // System.out.println(ex.v);
+        System.out.println("===== 수도 맞추기 게임을 시작합니다 =====");
+
+        // 입력받는 숫자를 저장할 gameNum 변수
+        // 입력받는 숫자를 저장할 gameNum 변수
+        int gameNum = 0;
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            System.out.println("입력: 1    퀴즈: 2   종료: 3");
+            gameNum = scan.nextInt();
+            if (gameNum == 3) {
+                System.out.println("게임을 종료합니다");
+                break;
+            } else if (gameNum == 2) {
+                ex.quiz();
+            } else if (gameNum == 1) {
+                ex.input();
+            } else {
+                System.out.println("다시 입력하세요");
+            }
+        }
+    }
 }
